80fee9f36291eea06883771f776db061118a7252
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-04-26 23:38:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+//  Q no 118
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+//  Q no 119
+var count = 0;
+while (count < 5) {
+    console.log("Hello World");
+    count++;
+}
+//  Q no 120
+var favouriteMovies = [
+    "Inception",
+    "The Matrix",
+    "Interstellar",
+    "The Prestige",
+];
+for (var _i = 0, favouriteMovies_1 = favouriteMovies; _i < favouriteMovies_1.length; _i++) {
+    var movie = favouriteMovies_1[_i];
+    console.log(movie);
+}
---
+++ main.ts
@@ -0,0 +1,25 @@
+//  Q no 118
+
+for(let i =1; i <=10; i++){
+console.log(i);
+}
+
+//  Q no 119
+
+let count:number = 0;
+while(count < 5){
+    console.log("Hello World");
+    count++;
+}
+
+//  Q no 120
+
+const favouriteMovies:string[] =[ 
+    "Inception",
+    "The Matrix",
+    "Interstellar",
+    "The Prestige",];
+
+    for(const movie of favouriteMovies){
+        console.log(movie);
+    }
